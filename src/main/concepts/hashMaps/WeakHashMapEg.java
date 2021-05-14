62af51f0386b31e3e957d5bf19eff16c03270f6b
Vamsi Tallapudi <vamsi@coderefer.com>
2021-05-14 12:31:01 +0530

    WeakHashMap Example

---
+++ src/main/concepts/hashMaps/WeakHashMapEg.java
@@ -0,0 +1,28 @@
+package main.concepts.hashMaps;
+
+import java.util.WeakHashMap;
+
+public class WeakHashMapEg {
+    public static void main(String[] args) throws Exception {
+        WeakHashMap<Person, Integer> hm = new WeakHashMap<>();
+        Person p = new Person();
+        Person q = new Person();
+
+        hm.put(p, 1);
+        hm.put(q, 1);
+        p = null; // making the person object eligible for gc
+        System.out.println("before: " + hm);
+        System.gc(); //calling garbage collector
+        Thread.sleep(1000);
+        System.out.println("after: " + hm);
+
+    }
+
+    static class Person {
+        @Override
+        protected void finalize() throws Throwable {
+            System.out.println("Calling finalize method");
+            super.finalize();
+        }
+    }
+}
